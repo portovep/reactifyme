06b907a62808fcae5634fb807dfb1899768d216b
Pablo Porto <portovep@tcd.ie>
2018-09-30 19:19:28 +0530

    Add accessibility storybook addon

--- src/storybook/config.js
+++ src/storybook/config.js
@@ -1,4 +1,5 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
@@ -6,6 +7,8 @@ import 'font-awesome/css/font-awesome.css';
 import '../App.css';
 
 const req = require.context('../../src', true, /\.stories\.js$/);
+
+addDecorator(checkA11y);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
